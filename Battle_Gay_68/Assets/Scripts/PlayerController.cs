df6cb4558c2cae5077dd8604e7e66fba519e5bc6
NichaKongkan <nicha.ko@kkumail.com>
2025-01-04 21:02:22 +0700

    player con move ja

--- Battle_Gay_68/Assets/Scripts/PlayerController.cs
+++ Battle_Gay_68/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
@@ -9,7 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
 
     private PlayerControls playerControls;
-    private Vector2 movement;
+    private UnityEngine.Vector2 movement;
     private Rigidbody2D rb;
 
     private void Awake() {
@@ -21,14 +23,15 @@ public class PlayerController : MonoBehaviour
         playerControls.Enable();
     }
 
+    private void Update() {
+        PlayerInput();
+    }
     private void FixedUpdate() {
         Move();
     }
-
     private void PlayerInput() {
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        movement = playerControls.Movement.Move.ReadValue<UnityEngine.Vector2>();
     }
-
     private void Move() {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
