d022e21e0b80dfc0d1b4dcdbcbae80dd5889a960
NichaKongkan <nicha.ko@kkumail.com>
2025-02-20 13:10:31 +0700

    edit dialogue (add response)

--- Battle_Gay_68/Assets/Scripts/DialogueUI.cs
+++ Battle_Gay_68/Assets/Scripts/DialogueUI.cs
@@ -5,10 +5,48 @@ using UnityEngine;
 
 public class DialogueUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject testDialogue;
+
+    private ResponseHandler responseHandler;
+    private TypewriterEffect typewriterEffect;
 
     private void Start()
     {
-        GetComponent<TypewriterEffect>().Run("This is a bit of\nHello", textLabel);
+        typewriterEffect = GetComponent<TypewriterEffect>();
+        responseHandler = GetComponent<ResponseHandler>();
+        CloseDialogueBox();
+        ShowDialogue(testDialogue);
+    }
+
+    public void ShowDialogue(DialogueObject dialogueObject) {
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject) {
+        for (int i = 0; i < dialogueObject.Dialogue.Length; i++) {
+            string dialogue = dialogueObject.Dialogue[i];
+            yield return typewriterEffect.Run(dialogue, textLabel);
+
+            if (i == dialogueObject.Dialogue.Length - 1 && dialogueObject.Responses != null && dialogueObject.HasResponses) break;
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+        }
+
+        if (dialogueObject.HasResponses) {
+            responseHandler.ShowResponse(dialogueObject.Responses);
+        } else {
+            CloseDialogueBox();
+        }
+        
+        
+
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox() {
+        dialogueBox.SetActive(false);
+        textLabel.text=string.Empty;
     }
 }
--- Battle_Gay_68/Assets/Scripts/TypewriterEffect.cs
+++ Battle_Gay_68/Assets/Scripts/TypewriterEffect.cs
@@ -4,16 +4,19 @@ using TMPro;
 using UnityEngine;
 
 public class TypewriterEffect : MonoBehaviour
-{
-    public void Run(string textToType, TMP_Text textLabel) {
-        StartCoroutine(TypeText(textToType, textLabel));
+{   
+    [SerializeField] private float typewriterEffect = 50f;
+    public Coroutine Run(string textToType, TMP_Text textLabel) {
+        return StartCoroutine(TypeText(textToType, textLabel));
     }
 
     private IEnumerator TypeText(string textToType, TMP_Text textLabel) {
+        textLabel.text = string.Empty;
+        
         float t = 0;
         int charIndex = 0;
         while (charIndex < textToType.Length) {
-            t += Time.deltaTime;
+            t += Time.deltaTime * typewriterEffect;
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
