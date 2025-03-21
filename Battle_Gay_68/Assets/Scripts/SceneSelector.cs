c2fdbe82f1163d1e5124388f7fda78bd69908501
NichaKongkan <nicha.ko@kkumail.com>
2025-03-21 23:41:05 +0700

    add choose ending

---
+++ Battle_Gay_68/Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour
+{
+    void Start()
+    {
+        CheckRelationshipAndWarp();
+    }
+
+    void CheckRelationshipAndWarp()
+    {
+        int boss1Relationship = PlayerPrefs.GetInt("RelationshipWithBoss_Boss_1", 0);
+        int boss2Relationship = PlayerPrefs.GetInt("RelationshipWithBoss_Boss_2", 0);
+        int boss3Relationship = PlayerPrefs.GetInt("RelationshipWithBoss_Boss_3", 0);
+
+
+        Debug.Log("Relation Boss1: " + boss1Relationship);
+        Debug.Log("Relation Boss2: " + boss2Relationship);
+        Debug.Log("Relation Boss3: " + boss3Relationship);
+
+        //>= 15
+        if ((boss1Relationship+boss2Relationship+boss3Relationship) >= 15)
+        {
+            SceneManager.LoadScene("Ending1");
+        }
+        else
+        {
+            Debug.Log("ไม่มีค่าความสัมพันธ์ถึงเกณฑ์ที่กำหนด");
+            SceneManager.LoadScene("Main Menu"); // ซีนเริ่มต้นถ้ายังไม่มีค่าครบตามเงื่อนไข
+        }
+    }
+}
